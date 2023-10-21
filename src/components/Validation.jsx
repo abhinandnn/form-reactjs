b7e54f30371ea47014aeff5867042a3bee9a0098
Abhinandan Pandey <abhinandan2210049@akgec.ac.in>
2023-10-21 23:01:36 +0530

    fixed the validations

--- src/components/Validation.jsx
+++ src/components/Validation.jsx
@@ -4,44 +4,64 @@ const useForm = () => {
 const [inputs, setInputs] = useState({
     firstname: "",
     lastname: "",
+    gender: "",
     email: "",
     studentno: "",
     rollno: "",
-    phone: "",
+    mobile: "",
     address: "",
     username: "",
     password: "",
     password2: ""
 });
-
+const [error,setError]  =  useState({
+  firstname: "",
+    lastname: "",
+    gender:"",
+    email: "",
+    studentno: "",
+    rollno: "",
+    mobile: "",
+    address: "",
+    username: "",
+    password: "",
+    password2: ""
+})
 
   const handleChange = e => {
     setInputs({
       ...inputs,
       [e.target.name]: e.target.value
     });
+    
   };
-
+  const email_valid= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const mobile_valid= /^((\+91)|(91)|0?)[6-9]\d{9}$/;
+  const rollno_valid= /^[1-2]\d{12}$/;
+  const studentno_valid= /^[1-2]\d{6}$/;
+  const valid_text=/^[a-zA-Z ]*$/;
+  const username_valid=/^[a-zA-Z][a-zA-Z0-9._]{4,}$/;
+  const password_valid=/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}/;
   const handleSubmit = e => {
     e.preventDefault();
-    const error = {
-    firstname: (inputs.firstname.trim()==='')|(inputs.firstname.length>15) ? 'lol' : '',
-    lastname: (inputs.lastname.length>15) ? 'lol' : '',
-    address: (inputs.address.trim()==='')|(inputs.address.length>20) ? 'lol' : '',
-    studentno: (inputs.studentno.trim()==='')|!(inputs.studentno.length===7) ? 'lol' : '',
-    rollno: (inputs.rollno.trim()==='')|!(inputs.rollno.length===13) ? 'lol' : '',
-    phone: (inputs.phone.trim() === '')|!(inputs.phone.length===10)? 'lol' : '',
-    email: (inputs.email.trim() === '')|(inputs.email.length>50)? 'lol' : '',
-    username: (inputs.username.trim()==='')|(inputs.username.length>15) ? 'lol' : '',
-    password: (inputs.password.trim() === '')|!(inputs.password===inputs.password2)|(inputs.password.length<8) ? 'lol' : '',
-    password2: inputs.password2.trim() === '' ? 'lol' : '',
-  }
+    setError( {
+    firstname: !valid_text.test(inputs.firstname.trim())|inputs.firstname.trim()===''|(inputs.firstname.length>15) ? 'Enter the First Name correctly!' : '',
+    lastname: !valid_text.test(inputs.lastname.trim())|(inputs.lastname.length>15) ? 'Enter the Last Name correctly!' : '',
+    gender: inputs.gender ? '':'Enter your gender',
+    address: !valid_text.test(inputs.address.trim())|inputs.address.trim()===''|(inputs.address.length>20) ? 'Enter the City Name correctly!' : '',
+    studentno: !studentno_valid.test(inputs.studentno.trim()) ? 'Enter the Student No correctly!' : '',
+    rollno: !rollno_valid.test(inputs.rollno.trim())? 'Enter the Roll No correctly!' : '',
+    mobile: !mobile_valid.test(inputs.mobile.trim())? 'Enter the Mobile No correctly!' : '',
+    email: (inputs.email.trim() === '')|!email_valid.test(inputs.email.trim())|(inputs.email.length>50)? 'Enter the email correctly!' : '',
+    username: !username_valid.test(inputs.username.trim())|(inputs.username.trim()==='')|(inputs.username.length>15) ? 'Enter the Username correctly!' : '',
+    password: !password_valid.test(inputs.password.trim())|(inputs.password.trim() === '')|(inputs.password.length<6) ? 'Password should contain atleast one letter and one number' : '',
+    password2: !(inputs.password===inputs.password2) ? 'Password is not matching!' : '',
+  })
     
       let hasErrors = false;
       for (const i in error) {
         if (error[i] !== '') {
           hasErrors = true;
-          break;
         }
       }
   
@@ -52,7 +72,7 @@ const [inputs, setInputs] = useState({
       console.log('fill all the details correctly!');
   };
 
-  return { handleChange, inputs, handleSubmit };
+  return { handleChange, inputs, handleSubmit, error };
 };
 
 export default useForm;
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,92 +3,110 @@ import Validation from "./Validation";
 
 
 function Form() {
-  const { handleChange, inputs, handleSubmit } = Validation();
+  const { handleChange, inputs, handleSubmit, error} = Validation();
 
   return (
     <div className="form">
       <form onSubmit={handleSubmit}>
+
         <div className="form__row">
+          <label>First Name:</label>
           <input
             type="text"
             name="firstname"
-            placeholder="First Name"
+            placeholder="Abhinandan"
             value={inputs.firstname}
             onChange={handleChange}
           />
-
+<p className="eror">{error.firstname}</p>
+<label>Last Name:</label>
           <input
             type="text"
             name="lastname"
-            placeholder="Last Name"
+            placeholder="Pandey"
             value={inputs.lastname}
             onChange={handleChange}
           />
+          <p className="eror">{error.lastname}</p>
+          <label>Gender:</label>
           <div className="gender" onChange={handleChange}>
-            <p className="gh">Gender:</p>
         <input type="radio" value="Male" name="gender" /> Male
         <input type="radio" value="Female" name="gender" /> Female
         <input type="radio" value="Other" name="gender" /> Other
       </div>
-
+        <p className="eror">{error.gender}</p>
+        <label>Email:</label>
           <input
             type="email"
             name="email"
-            placeholder="Email"
+            placeholder="abhinandan2210049@akgec.ac.in"
             value={inputs.email}
             onChange={handleChange}
-            
           />
+          <p className="eror">{error.email}</p>
+          <label>Student No:</label>
           <input
             type="number"
             name="studentno"
-            placeholder="Student No"
+            placeholder="Must be of 7 digits"
             value={inputs.studentno}
             onChange={handleChange}
           />
+          <p className="eror">{error.studentno}</p>
+          <label>Roll No:</label>
           <input
             type="number"
             name="rollno"
-            placeholder="Roll No"
+            placeholder="Must be of 13 digits"
             value={inputs.rollno}
             onChange={handleChange}
           />
-
+<p className="eror">{error.rollno}</p>
+<label>Mobile No:</label>
           <input
             type="number"
-            name="phone"
-            placeholder="Mobile No"
-            value={inputs.phone}
+            name="mobile"
+            placeholder="Must be of 10 digits"
+            value={inputs.mobile}
             onChange={handleChange}
           />
+          <p className="eror">{error.mobile}</p>
+          <label>City:</label>
           <input
             type="textarea"
             name="address"
-            placeholder="City"
+            placeholder="Lucknow"
             value={inputs.address}
             onChange={handleChange}
           />
+          <p className="eror">{error.address}</p>
+          <label>Username:</label>
           <input
             type="text"
             name="username"
-            placeholder="Username"
+            placeholder="Can contain english alphabets,numbers,'.'and'_'"
             value={inputs.username}
             onChange={handleChange}
           />
+          <p className="eror">{error.username}</p>
+          <label>Password:</label>
           <input
             type="password"
             name="password"
-            placeholder="Password"
+            placeholder="Must have 6 characters"
             value={inputs.password}
             onChange={handleChange}
           />
+          <p className="eror">{error.password}</p>
+          <label>Confirm Password</label>
           <input
             type="password"
             name="password2"
-            placeholder="Confirm Password *"
+            placeholder="Should be same as password"
             value={inputs.password2}
             onChange={handleChange}
           />
+          <p className="eror">{error.password2}</p>
           <button>SUBMIT</button>
         </div>
       </form>
